c0820ad327047af8522d4cfe0e8e0fe4bb4bf183
Stuti Sehgal <59798332+stutisehgal@users.noreply.github.com>
2021-05-29 20:24:28 +0530

    Depth First Search code in Python

---
+++ Depth First Search/dfs.py
@@ -0,0 +1,26 @@
+# DFS algorithm
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+graph = {'0': set(['1', '2','3']),
+         '1': set([]),
+         '2': set(['4']),
+         '3': set([]),
+         '4': set([])}
+
+'''graph = {'0': set(['1', '2']),
+'1': set(['0', '3', '4']),
+'2': set(['0']),
+'3': set(['1']),
+'4': set(['2', '3'])}'''
+
+dfs(graph, '0')
